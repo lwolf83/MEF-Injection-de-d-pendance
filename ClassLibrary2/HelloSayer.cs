95898e5dfa2174ec1467d21b75259fa8e833f44e
Laurent Wolf <lwolf.83@outlook.fr>
2020-04-06 12:25:07 +0200

    FC

---
+++ ClassLibrary2/HelloSayer.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.Composition;
+using System;
+
+namespace MusicApp
+{
+    [Export]
+    public class HelloSayer
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.Composition.Hosting;
+using System;
+
+namespace MusicApp
+{
+    class Program
+    {
+        private MefHost _host;
+
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            program.Run();
+        }
+
+        private void Run()
+        {
+            _host = new MefHost();
+            _host.Container.GetExportedValue<HelloSayer>().SayHello();
+        }
+    }
+
+    internal class MefHost
+    {
+        public CompositionContainer Container
+        {
+            get
+            {
+                if (_container == null)
+                {
+                    var catalog = new DirectoryCatalog(".", "MusicApp.*");
+                    _container = new CompositionContainer(catalog);
+                }
+                return _container;
+            }
+        }
+        private CompositionContainer _container = null;
+    }
+}
